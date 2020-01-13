56832ce8d4c272b4106cdb5f39b6978217aa5443
Stefan Warm <gustavo@stefans-air.localdomain>
2020-01-13 20:19:51 +0100

    Deploy website
    Deploy website version based on c217b286052e6c48548734629983fd437e1c0af8

--- js/load-mathjax.js
+++ js/load-mathjax.js
@@ -23,7 +23,7 @@ window.MathJax = {
 
 (function() {
     var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
     script.async = true;
     document.head.appendChild(script);
 })();
